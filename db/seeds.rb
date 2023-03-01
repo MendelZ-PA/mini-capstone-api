8527b55809ec660f0cbf173742665afda2c93b64
Mendel Zirkind <mendelzirkindfresno@gmail.com>
2023-02-28 21:51:44 -0500

    adds watermelon

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 Product.create!(name: "apple", price: 2, image_url: "https://pngtree.com/free-png-vectors/apple", description: "delicioius looking red apple")
 Product.create!(name: "orange", price: 1.5, image_url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2016%2F02%2F23%2F17%2F42%2Forange-1218158_1280.png&imgrefurl=https%3A%2F%2Fpixabay.com%2Fillustrations%2Ffruit-orange-png-transparent-1218158%2F&tbnid=yOz9TOImuKVuIM&vet=12ahUKEwi_xciH1bn9AhW8CVkFHbH8CToQMygGegUIARDxAQ..i&docid=aLcMNn_lTcNXpM&w=1280&h=1216&q=orange%20images%20png&ved=2ahUKEwi_xciH1bn9AhW8CVkFHbH8CToQMygGegUIARDxAQ", description: "orange with fruit based wax coating")
 Product.create!(name: "lemon", price: 0.25, image_url: "https://www.vecteezy.com/png/7303418-lemon-fruit-food-transparent-png", description: "myer lemon")
+Product.create!(name: "watermelon", price: 5, image_url: "https://pixabay.com/images/search/watermelon/", description: "seedless watermelon")
