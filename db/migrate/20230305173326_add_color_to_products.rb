aefc3ecb0f7425d28de8adc77402fc4cbc66d824
Mendel Zirkind <mendelzirkindfresno@gmail.com>
2023-03-05 13:19:20 -0500

    changes datatype

---
+++ db/migrate/20230305173326_add_color_to_products.rb
@@ -0,0 +1,8 @@
+class AddColorToProducts < ActiveRecord::Migration[7.0]
+  def change
+    change_column :products, :price, :decimal, percision: 9, scale: 2
+    change_column :products, :description, :text
+    change_column :products, :image_url, :text
+    add_column :products, :inventory, :integer
+  end
+end
