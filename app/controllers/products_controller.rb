1fcc6cba8bd35b53605c3a7c103d47d582bf61d5
Mendel Zirkind <mendelzirkindfresno@gmail.com>
2023-03-08 21:30:43 -0500

    added happy/sad paths

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,14 @@
 class ProductsController < ApplicationController
   def index
-    products = Product.all
-    render json: products.as_json
+    @products = Product.all
+    render template: "products/index"
+    render json: @products, :include => [:supplier]
   end
 
   def show
     product_id = params[:id]
     product = Product.find_by(id: product_id)
-    render json: product.as_json
+    render template: "products/show"
   end
 
   def create
@@ -17,18 +18,36 @@ class ProductsController < ApplicationController
       image_url: params[:image_url],
       description: params[:description],
     )
-    product.save
-    render json: product.as_json
+    if @product.save
+      # happy_path (ie successful)
+      render template: "products/show"
+    else
+      # sad_path (ie unsuccessful)
+      render json: { errors: @product.errors.full.messages }, status: :unprocessable_entity
+    end
   end
 
   def update
-    product = Product.find_by(id: params["id"])
-    product.update(
-      name: params["name"] || product.name,
-      price: params["price"] || product.price,
-      image_url: params["image_url"] || product.image_url,
-      description: params["description"] || product.description,
+    product_id = params[:id]
+    @product = Product.find(product_id)
+    @product.update(
+      name: params[:name] || @product.name,
+      price: params[:price] || @product.price,
+      image_url: params[:image_url] || @product.image_url,
+      description: params[:description] || @product.description,
     )
-    render json: product.as_json
+    if @product.valid?
+      #happy_path
+      render template: "products/show"
+    else
+      #sad_path
+      render json: { errors: @product.errors.full.messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    render json: { message: "Product successfully destroyed!" }
   end
 end
