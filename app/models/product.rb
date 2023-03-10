c564547d54fd82fcf2078c4d56309a5b2be3ee3f
Mendel Zirkind <mendelzirkindfresno@gmail.com>
2023-03-09 20:01:37 -0500

    added more code

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,9 @@ class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :description, length: { in: 10..500 }
+  # validates_format_of :image_url, :with => %r{\.(png|jpg|jpeg)$}i, :message => "Must have a file type of .png, .jpg, .jpeg", :multiline => true
+
+  def supplier
+    Supplier.find_by(id: supplier_id)
+  end  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,8 @@ Supplier.create!(name: "ABC Produce", email: "abcproduce@test.com", phone_number
 Supplier.create!(name: "Don's Lemons", email: "lemons@test.com", phone_number: "800-353-6667")
 Supplier.create!(name: "Washington Apples, LLC", email: "washingtonapples@test.com", phone_number: "800-927-7537")
 
-#apple: "https://pngtree.com/free-png-vectors/apple"
-#orange: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2016%2F02%2F23%2F17%2F42%2Forange-1218158_1280.png&imgrefurl=https%3A%2F%2Fpixabay.com%2Fillustrations%2Ffruit-orange-png-transparent-1218158%2F&tbnid=yOz9TOImuKVuIM&vet=12ahUKEwi_xciH1bn9AhW8CVkFHbH8CToQMygGegUIARDxAQ..i&docid=aLcMNn_lTcNXpM&w=1280&h=1216&q=orange%20images%20png&ved=2ahUKEwi_xciH1bn9AhW8CVkFHbH8CToQMygGegUIARDxAQ"
-#lemon: "https://www.vecteezy.com/png/7303418-lemon-fruit-food-transparent-png"
-#watermelon: "https://pixabay.com/images/search/watermelon/"
+
+Image.create!(image_url: "https://pngtree.com/free-png-vectors/apple", product_id: 1),
+Image.create!(image_url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2016%2F02%2F23%2F17%2F42%2Forange-1218158_1280.png&imgrefurl=https%3A%2F%2Fpixabay.com%2Fillustrations%2Ffruit-orange-png-transparent-1218158%2F&tbnid=yOz9TOImuKVuIM&vet=12ahUKEwi_xciH1bn9AhW8CVkFHbH8CToQMygGegUIARDxAQ..i&docid=aLcMNn_lTcNXpM&w=1280&h=1216&q=orange%20images%20png&ved=2ahUKEwi_xciH1bn9AhW8CVkFHbH8CToQMygGegUIARDxAQ", product_id: 2),
+Image.create!(image_url: "https://www.vecteezy.com/png/7303418-lemon-fruit-food-transparent-png", product_id: 3),
+Image.create!(image_url: "https://pixabay.com/images/search/watermelon/", product_id: 4)
